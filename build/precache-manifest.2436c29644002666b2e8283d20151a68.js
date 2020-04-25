425151d2ee5a307f5d222a691628c9d830b5f88b
Boon <admin@Admins-MacBook-Air.local>
2020-04-26 01:13:07 +0530

    build files

---
+++ build/precache-manifest.2436c29644002666b2e8283d20151a68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6c865a4e0788904618c12660726585a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e0d903126124a002bb61",
+    "url": "/monsters-rolodex/static/css/main.20dc5033.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0d903126124a002bb61",
+    "url": "/monsters-rolodex/static/js/main.667f22ab.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
