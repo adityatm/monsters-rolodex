4c67ea09df361cd82c4e3a62f806e65eeb16aa85
Boon <admin@Admins-MacBook-Air.local>
2020-04-26 00:05:16 +0530

    mosters files upload in github

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list.components';
+import { SearchBox } from './components/search-box/search-box-components';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+
+class App extends Component{
+  constructor(){
+    super();
+
+    this.state = {
+      monsters:[],
+      searchFeild: ''
+    };
+
+    // this.handleChange = this.handleChange.bind(this)
+}
+
+componentDidMount(){
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(users => this.setState({ monsters : users}));
+};
+
+
+handleChange = e => {
+  this.setState({ searchFeild : e.target.value})
+}
+
+
+  render(){
+    const { monsters, searchFeild } =  this.state;
+    const filterMonsters = monsters.filter(monster => 
+        monster.name.toLowerCase().includes(searchFeild.toLowerCase())
+      );
+
+
+    return(
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+        <SearchBox 
+          placeholder='Search Monster'
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filterMonsters} />
+
+
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <p>{this.state.name}</p>
+        <button 
+        onClick={()=>
+          this.setState({ name: 'Hello Lalboon Chauhan'})
+        }
+        >Change Text</button>
+      </header> */}
+    </div>      
+    )
+  }
 }
 
 export default App;
